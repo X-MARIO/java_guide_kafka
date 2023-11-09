38748e05412926024b4f728ab8b552614e7221b9
pchekushkin <pchekushkin@clink.tech>
2023-11-09 21:37:31 +0400

    init

---
+++ src/main/java/net/javaguides/springbootkafkatutorial/payload/User.java
@@ -0,0 +1,28 @@
+package net.javaguides.springbootkafkatutorial.payload;
+
+public class User {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
